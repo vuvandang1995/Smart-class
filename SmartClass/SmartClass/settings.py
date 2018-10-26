e71f343f7f8fb57d8bcfadf556111b05cc6472dc
dung1101 <37098514+dung1101@users.noreply.github.com>
2018-10-26 18:09:46 +0700

    Update settings.py

--- SmartClass/SmartClass/settings.py
+++ SmartClass/SmartClass/settings.py
@@ -155,4 +155,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ticketmdtpro@gmail.com'
+EMAIL_HOST_PASSWORD = '12345'
+EMAIL_PORT = 587
 
