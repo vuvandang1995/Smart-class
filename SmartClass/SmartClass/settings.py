b8ac830b36022f38f128f4f4a4211b35fd667604
dung1101 <37098514+dung1101@users.noreply.github.com>
2018-10-26 17:14:41 +0700

    Update settings.py

--- SmartClass/SmartClass/settings.py
+++ SmartClass/SmartClass/settings.py
@@ -155,8 +155,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'ticketmdtpro@gmail.com'
-# EMAIL_HOST_PASSWORD = 'meditech2018'
-EMAIL_PORT = 587
+
